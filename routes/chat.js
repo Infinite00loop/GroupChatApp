a709b109722923460a07845d2db2145fc848c4a5
Infinite00loop <samriddhisingh07092002@gmail.com>
2023-12-22 11:06:52 +0530

    message.txt created

--- routes/chat.js
+++ routes/chat.js
@@ -1,14 +1,17 @@
 const express= require('express');
+ const fs= require('fs');
 
 const router=express.Router();
 
 router.get('/',(req,res,next)=>{
     res.send(
-        '<form action="/" method="POST" ><input type="text" name="Chat" ><button type="submit">Send</button></form>')
+        '<form action="/" method="POST" ><input type="text" name="chat" ><button type="submit">Send</button></form>')
 
 });
     
 router.post('/',(req,res,next)=>{
+    const message=req.body.chat;
+    fs.appendFileSync('message.txt',`${message}\n`);
     console.log(req.body);
     res.redirect('/');
 });
